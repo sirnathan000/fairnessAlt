956bfc14002af3332224aae082f9c03b0475dd61
sirnathan000 <nathanmitchel@aol.nl>
2025-01-15 16:49:09 +0000

    added extra to do need to seriously consider just using the fairness directery instead

--- modified/datasets/__init__.py
+++ modified/datasets/__init__.py
@@ -8,6 +8,7 @@ def get_dataset_params(name):
         IS_CAT = [True, False, True, True, True, True, True, True]
         max_numeric = {"age": 50.5}
     elif name == Dataset.ADULT2:
+        #todo: add the necessary information to make the ratio work alternatively run the ratio here and pass it allong (dumb thing to do but hey if it works it works)
         QI_INDEX = [1, 2, 3, 4, 5, 6, 7, 8]
         target_var = 'salary-class'
         IS_CAT = [True, False, True, True, True, True, True, True]
